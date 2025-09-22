8b220d77eb79bfb599c86ed1381483ebbd13b6bd
Rajkiran1408 <rameshsundarraj72@gmail.com>
2025-09-22 20:59:47 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
-})
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/my-portfolio",
+});
 
 
